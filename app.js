eb15cabad814c8e5579c724e20a9e16e52e6d547
filipeoliveiraanalytics <fd.olive.23@gmail.com>
2026-08-22 11:08:48 +0200

    Updated after FP1 and SQ1

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const ASSET_VERSION = "20260821-netherlands-preweekend";
+const ASSET_VERSION = "20260822-netherlands-after-practice";
 const DATA_PATH = `data/fantasy_projections.csv?v=${ASSET_VERSION}`;
 const PRICE_MOVEMENTS_PATH = `data/fantasy_price_movements.csv?v=${ASSET_VERSION}`;
 const CONSENT_KEY = "gp_fantasy_predictor_analytics_consent";
