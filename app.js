0352c1365eb08d9f5796cd352f30d9d824a10203
filipeoliveiraanalytics <fd.olive.23@gmail.com>
2026-09-14 12:44:14 +0200

    Baku update

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const ASSET_VERSION = "20260912-post-quali";
+const ASSET_VERSION = "20260914-madrid-scored-baku-pre";
 const DATA_PATH = `data/fantasy_projections.csv?v=${ASSET_VERSION}`;
 const PRICE_MOVEMENTS_PATH = `data/fantasy_price_movements.csv?v=${ASSET_VERSION}`;
 const FORECAST_TRACKER_PATH = `data/fantasy_forecast_tracker.csv?v=${ASSET_VERSION}`;
